1169d813883af5fa14c04ed9c770d8bd3faf07fa
axumfacts-png <axumfacts@gmail.com>
2026-06-19 08:56:52 -1200

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,3 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN missing")
-
-if not GEMINI_API_KEY:
-    raise ValueError("GEMINI_API_KEY missing")
